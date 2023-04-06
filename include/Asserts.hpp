47158e271def5b8d06828a9490033dda5205812b
Samo Chreno <samochreno@gmail.com>
2023-04-06 18:18:04 +0200

    Make colorful log messages cross platform and use a custom assert macro

--- include/Asserts.hpp
+++ include/Asserts.hpp
@@ -1,7 +1,24 @@
 #pragma once
 
-#include <cassert>
 #include <exception>
 
-#define ACE_ASSERT(t_condition) assert(t_condition)
-#define ACE_UNREACHABLE() assert(false); std::terminate()
+#include "Log.hpp"
+
+#define ACE_ASSERT(t_condition) \
+    if (!(t_condition)) \
+    { \
+        ACE_LOG_ERROR( \
+            "Assertion failed in file " << __FILE__ << \
+            " at line " << __LINE__ << ":" << #t_condition \
+        ); \
+        ACE_LOG_FLUSH(); \
+        std::terminate(); \
+    } static_assert(true, "Semicolon required.")
+
+#define ACE_UNREACHABLE() \
+    ACE_LOG_ERROR( \
+        "Reached unreachable location in file " << __FILE__ << \
+        " at line " << __LINE__ \
+    ); \
+    ACE_LOG_FLUSH(); \
+    std::terminate()
--- include/Log.hpp
+++ include/Log.hpp
@@ -1,7 +1,11 @@
 #pragma once
 
 #include <iostream>
+#include <ostream>
+#include <termcolor/termcolor.hpp>
 
 #define ACE_LOG_EMPTY() std::cout << "\n"
-#define ACE_LOG_INFO(t_message) std::cout << "\033[90m<Info>\033[37m " << t_message << "\033[37m\n"
-#define ACE_LOG_WARNING(t_message) std::cout << "\033[33m<Warning>\033[93m " << t_message << "\033[37m\n"
+#define ACE_LOG_INFO(t_message) std::cout << termcolor::bright_grey << "<Info> " << termcolor::reset << t_message << "\n"
+#define ACE_LOG_WARNING(t_message) std::cout << termcolor::yellow << "<Warning> " << termcolor::reset << t_message << "\n"
+#define ACE_LOG_ERROR(t_message) std::cout << termcolor::red << "<Error> " << termcolor::reset << t_message << "\n"
+#define ACE_LOG_FLUSH(t_message) std::cout << std::flush
