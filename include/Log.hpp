939b59de029eecdd8b075c93bd38067e09a63036
Samo Chreno <samochreno@gmail.com>
2023-04-07 16:00:58 +0200

    Make logs pretty

--- include/Log.hpp
+++ include/Log.hpp
@@ -4,8 +4,37 @@
 #include <ostream>
 #include <termcolor/termcolor.hpp>
 
-#define ACE_LOG_EMPTY() std::cout << "\n"
-#define ACE_LOG_INFO(t_message) std::cout << termcolor::bright_grey << "<Info> " << termcolor::reset << t_message << "\n"
-#define ACE_LOG_WARNING(t_message) std::cout << termcolor::yellow << "<Warning> " << termcolor::reset << t_message << "\n"
-#define ACE_LOG_ERROR(t_message) std::cout << termcolor::red << "<Error> " << termcolor::reset << t_message << "\n"
+namespace Ace::Log
+{
+    auto CreateTimestamp() -> std::string;
+}
+
+#define ACE_LOG_COLOR_WHITE       255, 255, 255
+#define ACE_LOG_COLOR_TIMESTAMP    44,  62,  80
+#define ACE_LOG_COLOR_INFORMATION  38, 162, 105
+#define ACE_LOG_COLOR_WARNING     243, 156,  18
+#define ACE_LOG_COLOR_ERROR       192,  57,  43
+
+#define ACE_LOG_EMPTY()          std::cout << "\n"
 #define ACE_LOG_FLUSH(t_message) std::cout << std::flush
+
+#define ACE_LOG_INFO(t_message) \
+    std::cout << termcolor::color<ACE_LOG_COLOR_WHITE> << termcolor::on_color<ACE_LOG_COLOR_INFORMATION> << \
+    " | INFORMATION  " << termcolor::on_color<ACE_LOG_COLOR_TIMESTAMP> << \
+    " " << Ace::Log::CreateTimestamp() << " " << termcolor::on_color<ACE_LOG_COLOR_INFORMATION> << \
+    " " << termcolor::reset << termcolor::color<ACE_LOG_COLOR_INFORMATION> << \
+    " " << t_message << "\n"
+
+#define ACE_LOG_WARNING(t_message) \
+    std::cout << termcolor::color<ACE_LOG_COLOR_WHITE> << termcolor::on_color<ACE_LOG_COLOR_WARNING> << \
+    " | WARNING      " << termcolor::on_color<ACE_LOG_COLOR_TIMESTAMP> << \
+    " " << Ace::Log::CreateTimestamp() << " " << termcolor::on_color<ACE_LOG_COLOR_WARNING> << \
+    " " << termcolor::reset << termcolor::color<ACE_LOG_COLOR_WARNING> << \
+    " " << t_message << "\n"
+
+#define ACE_LOG_ERROR(t_message) \
+    std::cout << termcolor::color<ACE_LOG_COLOR_WHITE> << termcolor::on_color<ACE_LOG_COLOR_ERROR> << \
+    " | ERROR        " << termcolor::on_color<ACE_LOG_COLOR_TIMESTAMP> << \
+    " " << Ace::Log::CreateTimestamp() << " " << termcolor::on_color<ACE_LOG_COLOR_ERROR> << \
+    " " << termcolor::reset << termcolor::color<ACE_LOG_COLOR_ERROR> << \
+    " " << t_message << "\n"
---
+++ src/Log.cpp
@@ -0,0 +1,17 @@
+#include "Log.hpp"
+
+#include <string>
+#include <ctime>
+
+namespace Ace::Log
+{
+    auto CreateTimestamp() -> std::string
+    {
+        time_t now = time(0);
+        struct tm tstruct;
+        char buf[80];
+        tstruct = *localtime(&now);
+        strftime(buf, sizeof(buf), "%Y-%m-%d.%X", &tstruct);
+        return buf;
+    }
+}
