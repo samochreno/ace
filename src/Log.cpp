26f58851204933b4e0fac4000b4f1883d00fde09
Samo Chreno <samochreno@gmail.com>
2023-04-07 16:17:17 +0200

    Change formatting of logging timestamp

--- src/Log.cpp
+++ src/Log.cpp
@@ -11,7 +11,7 @@ namespace Ace::Log
         struct tm tstruct;
         char buf[80];
         tstruct = *localtime(&now);
-        strftime(buf, sizeof(buf), "%Y-%m-%d.%X", &tstruct);
+        strftime(buf, sizeof(buf), "%Y-%m-%d %X", &tstruct);
         return buf;
     }
 }
