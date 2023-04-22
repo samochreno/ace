3001b3d02be38aec0bfdf9c9ad88c34b02bc96ac
Samo Chreno <samochreno@gmail.com>
2023-04-22 19:01:32 +0200

    Edit whitespace

--- ace/generator.py
+++ ace/generator.py
@@ -1,44 +1,55 @@
 ITERATION_COUNT = 150
 
-TEXT = """
+TEXT = """\
 $Vector2: struct {
     x: int,
     y: int,
 }
+
 $compound(): int {
     value: $Vector2 = new $Vector2{ x: 3, y: -27 };
+
     value.y -= 1;
     print_int(value.y);
+
     boxed_value: *$Vector2 = box value;
     boxed_value.x *= 2;
     print_int(boxed_value.x);
+
     ret 0;
 }
+
 $precedence(): int {
     i: int = -$value();
     print_int(i);
     ret 0;
 }
+
 $value(): int {
     ret -131255;
 }
+
 $assoc(): int {
     ret 1 * -100 + 50 / 10 / 5;
 }
+
 $Getter: struct {
     field: int,
 }
+
 impl $Getter {
     field(): int -> self {
         ret self.field;
     }
 }
+
 $getter(): int {
     print_int(15);
     obj: $Getter = new $Getter { field: $value() };
     print_int(60);
     ret obj.field();
 }
+
 """
 
 IDENTIFIERS = [
