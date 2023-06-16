cbe9702ed73b93d992fa2be802b98ce791c8ff27
samochreno <samochreno@gmail.com>
2023-06-16 22:09:06 +0200

    Add severity

--- include/DiagnosticBag.hpp
+++ include/DiagnosticBag.hpp
@@ -3,6 +3,9 @@
 #include <memory>
 #include <vector>
 
+#include "Asserts.hpp"
+#include "DiagnosticsBase.hpp"
+
 namespace Ace
 {
     template<typename TDiagnostic>
@@ -12,35 +15,70 @@ namespace Ace
         DiagnosticBag() = default;
         ~DiagnosticBag() = default;
 
+        auto Add(const std::shared_ptr<const TDiagnostic>& t_diagnostic) -> void
+        {
+            m_Diagnostics.push_back(t_diagnostic);
+            AddSeverity(t_diagnostic->GetSeverity());
+        }
         template<typename TDiagnosticNew, typename... A>
         auto Add(A&&... args) -> void
         {
-            m_Diagnostics.push_back(std::make_shared<TDiagnosticNew>(
+            Add(std::make_shared<TDiagnosticNew>(
                 std::forward<A>(args)...
             ));
-        }
-        template<typename TDiagnosticNew>
-        auto Add(
-            const std::shared_ptr<const TDiagnosticNew>& t_diagnostic
-        ) -> void
-        {
-            m_Diagnostics.push_back(t_diagnostic);
         }
         template<typename TDiagnosticNew>
         auto Add(
             const DiagnosticBag<TDiagnosticNew>& t_diagnosticBag
         ) -> void
         {
-            m_Diagnostics.insert(
-                end(m_Diagnostics),
+            std::for_each(
                 begin(t_diagnosticBag.GetDiagnostics()),
-                end  (t_diagnosticBag.GetDiagnostics())
+                end  (t_diagnosticBag.GetDiagnostics()),
+                [&](const std::shared_ptr<const TDiagnosticNew>& t_diagnostic)
+                {
+                    Add(t_diagnostic);
+                }
             );
         }
 
         auto GetDiagnostics() const -> const std::vector<std::shared_ptr<const TDiagnostic>>& { return m_Diagnostics; }
+        auto GetSeverity() const -> DiagnosticSeverity { return m_Severity; }
 
     private:
+        auto AddSeverity(const DiagnosticSeverity& t_severity) -> void
+        {
+            switch (t_severity)
+            {
+                case DiagnosticSeverity::Info:
+                {
+                    break;
+                }
+
+                case DiagnosticSeverity::Warning:
+                {
+                    if (m_Severity == DiagnosticSeverity::Info)
+                    {
+                        m_Severity = DiagnosticSeverity::Warning;
+                    }
+
+                    break;
+                }
+
+                case DiagnosticSeverity::Error:
+                {
+                    m_Severity = DiagnosticSeverity::Error;
+                    break;
+                }
+
+                default:
+                {
+                    ACE_UNREACHABLE();
+                }
+            }
+        }
+
         std::vector<std::shared_ptr<const TDiagnostic>> m_Diagnostics{};
+        DiagnosticSeverity m_Severity = DiagnosticSeverity::Info;
     };
 }
