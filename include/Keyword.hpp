b9a5e4ded6d2018ed9d0e9e5544cbf986509c27f
samochreno <samochreno@gmail.com>
2023-07-23 00:22:28 +0200

    Fix formatting

--- include/Keyword.hpp
+++ include/Keyword.hpp
@@ -4,41 +4,41 @@ namespace Ace
 {
     namespace Keyword
     {
-        inline constexpr const char* AddressOf  = "__address_of";
-        inline constexpr const char* SizeOf     = "__size_of";
-        inline constexpr const char* DerefAs    = "__deref_as";
+        inline constexpr const char* AddressOf = "__address_of";
+        inline constexpr const char* SizeOf    = "__size_of";
+        inline constexpr const char* DerefAs   = "__deref_as";
 
-        inline constexpr const char* If         = "if";
-        inline constexpr const char* Else       = "else";
-        inline constexpr const char* Elif       = "elif";
-        inline constexpr const char* While      = "while";
-        inline constexpr const char* Module     = "module";
-        inline constexpr const char* Return     = "ret";
-        inline constexpr const char* Struct     = "struct";
-        inline constexpr const char* Op         = "op";
-        inline constexpr const char* Public     = "pub";
-        inline constexpr const char* True       = "true";
-        inline constexpr const char* False      = "false";
-        inline constexpr const char* Extern     = "extern";
-        inline constexpr const char* Cast       = "cast";
-        inline constexpr const char* Exit       = "exit";
-        inline constexpr const char* Assert     = "assert";
-        inline constexpr const char* Impl       = "impl";
-        inline constexpr const char* Box        = "box";
-        inline constexpr const char* Unbox      = "unbox";
+        inline constexpr const char* If     = "if";
+        inline constexpr const char* Else   = "else";
+        inline constexpr const char* Elif   = "elif";
+        inline constexpr const char* While  = "while";
+        inline constexpr const char* Module = "module";
+        inline constexpr const char* Return = "ret";
+        inline constexpr const char* Struct = "struct";
+        inline constexpr const char* Op     = "op";
+        inline constexpr const char* Public = "pub";
+        inline constexpr const char* True   = "true";
+        inline constexpr const char* False  = "false";
+        inline constexpr const char* Extern = "extern";
+        inline constexpr const char* Cast   = "cast";
+        inline constexpr const char* Exit   = "exit";
+        inline constexpr const char* Assert = "assert";
+        inline constexpr const char* Impl   = "impl";
+        inline constexpr const char* Box    = "box";
+        inline constexpr const char* Unbox  = "unbox";
 
-        inline constexpr const char* Int8       = "i8";
-        inline constexpr const char* Int16      = "i16";
-        inline constexpr const char* Int32      = "i32";
-        inline constexpr const char* Int64      = "i64";
-        inline constexpr const char* UInt8      = "u8";
-        inline constexpr const char* UInt16     = "u16";
-        inline constexpr const char* UInt32     = "u32";
-        inline constexpr const char* UInt64     = "u64";
-        inline constexpr const char* Float32    = "f32";
-        inline constexpr const char* Float64    = "f64";
-        inline constexpr const char* Bool       = "bool";
-        inline constexpr const char* Void       = "void";
-        inline constexpr const char* Int        = "int";
+        inline constexpr const char* Int8    = "i8";
+        inline constexpr const char* Int16   = "i16";
+        inline constexpr const char* Int32   = "i32";
+        inline constexpr const char* Int64   = "i64";
+        inline constexpr const char* UInt8   = "u8";
+        inline constexpr const char* UInt16  = "u16";
+        inline constexpr const char* UInt32  = "u32";
+        inline constexpr const char* UInt64  = "u64";
+        inline constexpr const char* Float32 = "f32";
+        inline constexpr const char* Float64 = "f64";
+        inline constexpr const char* Bool    = "bool";
+        inline constexpr const char* Void    = "void";
+        inline constexpr const char* Int     = "int";
     };
 }
